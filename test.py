c039484f78b2126147ba4b0cb360aef8ba65a049
Enrico-Medina <161720504+Enrico-Medina@users.noreply.github.com>
2024-06-23 23:28:26 -0300

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="MU Fest")
 st.title("Bem-vindo ao MU Fest!")
-st.write("Explore os eventos e aproveite ao máximo!")
+st.write("Explore os melhores eventos e aproveite ao máximo!")
 
 # Login
 st.title("Login")
